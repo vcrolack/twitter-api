f48eb4f72179eb3a5300f7c5379232412d8bd0c4
Victor Rolack <victorolack@gmail.com>
2022-05-20 00:28:07 -0400

    created users models

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+#Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+#FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+#########################
+#       MODELS          #
+#########################
+
+class User(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=20
+    )
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_day: Optional[date] = Field(default=None)
+
+class Twett(BaseModel):
+    pass
 
 
 @app.get(path="/")
 def home():
     return {"Twitter API": "Working"}
-
-
 
 
 
