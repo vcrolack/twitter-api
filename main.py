e340fb3c80d9aa9e0af008b5d44f33b13dc8bb89
Victor Rolack <victorolack@gmail.com>
2022-05-20 00:46:37 -0400

    created tweet model

--- main.py
+++ main.py
@@ -1,52 +1,18 @@
 #Python
-from uuid import UUID
-from datetime import date
-from typing import Optional
 
 #Pydantic
-from pydantic import BaseModel
-from pydantic import EmailStr
-from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
 
+#Models 
+from models import User, UserBase, UserLogin, Tweet
+
 app = FastAPI()
-
-#########################
-#       MODELS          #
-#########################
-
-class User(BaseModel):
-    user_id: UUID = Field(...)
-    email: EmailStr = Field(...)
-    password: str = Field(
-        ...,
-        min_length=8,
-        max_length=20
-    )
-    first_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50
-    )
-    last_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50
-    )
-    birth_day: Optional[date] = Field(default=None)
-
-class Twett(BaseModel):
-    pass
-
 
 @app.get(path="/")
 def home():
     return {"Twitter API": "Working"}
-
-
-
 
 if __name__ == "__main__":
     import uvicorn
